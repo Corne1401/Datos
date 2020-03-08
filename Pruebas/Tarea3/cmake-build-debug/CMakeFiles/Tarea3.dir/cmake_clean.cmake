9bec2675eb97291d839c2f8707dbf4f625e72156
Daniel <danicor14@hotmail.com>
2020-03-08 13:39:24 -0600

    Tarea 3 y carpeta de pruebas

---
+++ Pruebas/Tarea3/cmake-build-debug/CMakeFiles/Tarea3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tarea3.dir/main.c.o"
+  "Tarea3"
+  "Tarea3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Tarea3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
